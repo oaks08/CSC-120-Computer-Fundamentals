36424c784e798963da8b6a4dc0805d2b904d2da2
Zidane Madlala <zmadlala9@gmail.com>
2021-05-10 15:41:46 -0400

    Add files via upload

---
+++ MadlalaLab02P1.py
@@ -0,0 +1,12 @@
+num = float(input("Enter a number: "))
+
+if num < 0:
+    print("This number is negative")
+
+
+if (num % 2) == 0:
+    print("{0} is Even number".format(num))
+else:
+    print("{0} is Odd number".format(num))
+
+
